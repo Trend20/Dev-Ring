314f847a36eeb39198b0893f8ac3586489ebdb46
Trend20 <enockomondi305@gmail.com>
2023-10-29 14:15:50 +0300

    adding the hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Hero from './components/Hero';
 
 function App() {
   return (
     <div className="app">
-      <h1>DevRing</h1>
+      <Hero />
     </div>
   );
 }
