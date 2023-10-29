8beddec657966b29f587bfc20c017e86b64dd402
Trend20 <enockomondi305@gmail.com>
2023-10-29 14:21:26 +0300

    adding about component

---
+++ src/components/About.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = (): JSX.Element => {
+  return (
+    <div className="flex">
+      <h3>Never Code Alone</h3>
+      <p>
+        Wanna supercharge your coding experience while making long-lasting
+        connections? Our platform makes it easy to find local developer
+        communities that share your passions and drive. Dare to thrive with
+        like-minded peers, learn together, and build amazing projects!
+      </p>
+    </div>
+  );
+};
+
+export default About;
