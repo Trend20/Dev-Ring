ad57b360d3a1453b9b404d3a100be64548d478e0
Trend20 <enockomondi305@gmail.com>
2023-10-30 18:09:27 +0300

    configuring routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import About from './components/About';
 import Features from './components/Features';
 import Hero from './components/Hero';
 import StatsCards from './components/StatsCards';
 import ReadToJoin from './components/ReadToJoin';
 import Navbar from './components/Navbar';
+import Waitlist from './pages/Waitlist';
 
 function App() {
   return (
-    <div className="app">
+    <BrowserRouter>
+      <div className="app">
       <Navbar />
       <Hero />
       <About />
@@ -16,6 +23,10 @@ function App() {
       <Features />
       <ReadToJoin />
     </div>
+    <Routes>
+      <Route path='/waitlist' element={<Waitlist />} />
+    </Routes>
+    </BrowserRouter>
   );
 }
 
