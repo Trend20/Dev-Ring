4ff4a3d3a973ec59eff5c84603a0e0e3750f314b
Trend20 <enockomondi305@gmail.com>
2023-10-30 10:05:33 +0300

    adding a navbar

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ import Features from './components/Features';
 import Hero from './components/Hero';
 import StatsCards from './components/StatsCards';
 import ReadToJoin from './components/ReadToJoin';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="app">
+      <Navbar />
       <Hero />
       <About />
       <StatsCards />
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,12 @@
-import { CgCommunity} from 'react-icons/cg'
+import { FaLifeRing} from 'react-icons/fa'
 
 const Navbar = ():JSX.Element => {
   return (
-    <div className='flex w-full items-center'>
-      <div className="flex">
-        <i><CgCommunity /></i>
+    <div className='flex w-full items-center justify-between bg-cyan-200 px-16'>
+      <div className="flex w-80 items-center">
+        <i><FaLifeRing size={50}/></i>
       </div>
-      <button className='flex'>Get Started</button>
+      <button className='bg-black rounded-br-lg text-cyan-300 font-bold p-3 mt-5 justify-center items-center flex w-40'>Get Started</button>
     </div>
   )
 }
