66ee68da7b84cf5c3b86680bf6044de809b774eb
Trend20 <enockomondi305@gmail.com>
2023-11-03 11:51:16 +0300

    auth page styles

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,6 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
-import About from './components/About';
-import Features from './components/Features';
-import Hero from './components/Hero';
-import StatsCards from './components/StatsCards';
-import ReadToJoin from './components/ReadToJoin';
-import Navbar from './components/Navbar';
-import Waitlist from './pages/Waitlist';
 import Auth from './pages/Auth';
 import Home from './pages/Home';
 
@@ -20,7 +13,6 @@ function App() {
     <div className="app min-h-screen">
     <Routes>
       <Route path="/" element={<Home />}/>
-      {/* <Route path='/waitlist' element={<Waitlist />} /> */}
       <Route path='/auth' element={<Auth />} />
     </Routes>
     </div>
--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -5,7 +5,9 @@ import {
   signInWithPopup,
   signOut,
 } from "firebase/auth";
-import { FaLifeRing} from 'react-icons/fa'
+import { FaLifeRing } from "react-icons/fa";
+import { FcGoogle} from 'react-icons/fc'
+import commImg from "../assets/images/comm.jpg";
 
 const Auth = (): JSX.Element => {
   const [email, setEmail] = useState("");
@@ -40,32 +42,34 @@ const Auth = (): JSX.Element => {
     }
   };
   return (
-    <div className="flex w-1/2 m-auto bg-cyan-300 justify-between items-centre rounded">
-      <div className="flex flex-col justify-center items-center w-1/2 p-10 m-auto h-auto text-cyan-300 bg-white">
-      <div className="flex items-center flex-col">
-        <i><FaLifeRing size={50} fill="#000"/></i>
-        <p className="text-black font-bold">DevRing</p>
-      </div>
+    <div className="flex w-full m-auto bg-cyan-300 justify-between items-centre rounded">
+      <div className="flex flex-col justify-center items-center w-1/2 p-10 m-auto min-h-screen text-cyan-300 bg-white">
+        <div className="flex items-center flex-col">
+          <i>
+            <FaLifeRing size={50} fill="#000" />
+          </i>
+          <p className="text-black font-bold">DevRing</p>
+        </div>
         <form
           onSubmit={Login}
           className="flex flex-col w-full justify-center items-center mt-14"
         >
-          <div className="flex w-full">
+          <div className="flex w-3/4">
             <input
               type="text"
               placeholder="Email..."
               value={email}
               onChange={(e) => setEmail(e.target.value)}
-              className="flex rounded border px-6 py-3 w-full"
+              className="flex rounded border px-6 py-3 w-full text-black outline-none"
             />
           </div>
-          <div className="flex w-full">
+          <div className="flex w-3/4">
             <input
               type="password"
               placeholder="Password..."
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              className="flex rounded border px-6 py-3 mt-10 w-full"
+              className="flex rounded border px-6 py-3 mt-10 w-full text-black outline-none"
             />
           </div>
           <button
@@ -77,12 +81,14 @@ const Auth = (): JSX.Element => {
         </form>
         <button
           onClick={googleLogin}
-          className="flex justify-center items-center border w-60 mt-5 p-2"
+          className="flex justify-center items-center border w-3/4 uppercase text-sm mt-12 font-bold text-black p-2"
         >
+          <i className="px-6"><FcGoogle  size={40}/></i>
           Sign In with Google
         </button>
       </div>
-      <div className="flex w-3/4">
+      <div className="flex w-3/4 min-h-screen justify-center items-center flex-col">
+        <img src={commImg} alt="login" />
         <p>We connect you to your local tech communities and events!</p>
       </div>
     </div>
