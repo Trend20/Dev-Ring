bd846e9124505f20366019c681fbb99cc7e51d30
Trend20 <enockomondi305@gmail.com>
2023-11-09 17:41:55 +0300

    update profile component

--- src/components/UpdateProfile.tsx
+++ src/components/UpdateProfile.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import profileImg from '../assets/images/pic.jpeg'
 
 const UpdateProfile = ():JSX.Element => {
   return (
-    <div className='flex w-full bg-cyan-200 min-h-screen'>
-        <h3>Profile Page</h3>
+    <div className='flex w-full bg-cyan-200 min-h-screen justify-center pt-10'>
+        <div className="flex flex-col items-center">
+        <img src={profileImg} alt="profile" className="flex w-40 h-40 rounded-full"/>
+        <h5 className='mt-5'>Dev Enock</h5>
+        <div className="flex py-5 w-96 justify-center items-center">
+          <p className='px-6'>Developer</p>
+          <p className='px-6'>Nairobi</p>
+          <p className='px-6'>Joined 2023</p>
+        </div>
+        </div>
+        <div className="flex">
+          
+        </div>
     </div>
   )
 }
