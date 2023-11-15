4d7999a8cfeefe8006af69fbd1a383639e23683b
Trend20 <enockomondi305@gmail.com>
2023-11-15 09:49:08 +0300

    navbar fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = ():JSX.Element => {
   const navigate = useNavigate();
   return (
     <div className='flex w-full items-center justify-between bg-cyan-200 px-16 max-md:w-full max-md:px-6 max-md:py-6'>
-      <div className="flex w-80 items-center md:w-60 max-md:items-start">
+      <div className="flex w-80 items-center cursor-pointer md:w-60 max-md:items-start" onClick={() => navigate('/')}>
         <i><FaLifeRing size={50}/></i>
       </div>
       <button onClick={() => navigate('/auth')} className='bg-black rounded-br-lg text-cyan-300 font-bold p-3 mt-5 justify-center items-center flex w-40 max-md:w-36 max-md:text-sm'>Get Started</button>
