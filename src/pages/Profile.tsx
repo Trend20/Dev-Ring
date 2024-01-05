ec6c58e4776ab235ea2fad936ae1692025cff157
Trend20 <enockomondi305@gmail.com>
2024-01-05 17:12:07 +0300

    profile section fix

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import profileImg from "../assets/images/pic.jpeg";
-import Communities from '../components/Communities';
+import Communities from "../components/Communities";
 import Events from "../components/Events";
 import Favorites from "../components/Favorites";
 import Archived from "../components/Archived";
@@ -9,33 +9,9 @@ const Profile = () => {
   const [openTab, setOpenTab] = useState<string>("communities");
 
   const handleTabOpen = (tabCategory: string) => {
-      setOpenTab(tabCategory);
+    setOpenTab(tabCategory);
   };
-  const [open, setOpen] = useState(false);
-  const handleOpen = () => setOpen((cur) => !cur);
 
-  const data = [
-    {
-      label: "My Communities",
-      value: "html",
-      content:"Yes"
-    },
-    {
-      label: "My Events",
-      value: "react",
-      content:"Yes"
-    },
-    {
-      label: "My Favorites",
-      value: "vue",
-      content:"Yes"
-    },
-    {
-      label: "Archived",
-      value: "angular",
-      content:"Yes"
-    },
-  ];
   return (
     <div className="flex bg-cyan-200 flex-col h-auto w-full pt-10">
       <div className="flex flex-col justify-center items-center w-full h-auto">
@@ -47,62 +23,62 @@ const Profile = () => {
         <h5>Dev Enock</h5>
         <p>enockdevring@gmail.com</p>
       </div>
-      <section className="py-5 dark:bg-dark">
-                <div className="container">
-                    <div className="-mx-4 flex flex-wrap">
-                        <div className="w-full px-4">
-                            <div className="mb-14 w-full">
-                                <div className="flex flex-col justify-between w-full  flex-wrap rounded-full border border-blue-gray-200 px-2 py-2 dark:border-dark-3 sm:flex-row">
-                                    <a
-                                        onClick={() => handleTabOpen("communities")}
-                                        className={`cursor-pointer rounded-full px-4 py-3 text-sm w-32 bg-blue-gray-50 items-center justify-center flex font-medium md:text-base lg:px-6 ${
-                                            openTab === "communities"
-                                                ? "bg-boxdark text-white"
-                                                : "text-body-color hover:bg-black hover:text-white dark:text-dark-6 dark:hover:text-white"
-                                        }`}
-                                    >
-                                        Communities
-                                    </a>
-                                    <a
-                                        onClick={() => handleTabOpen("events")}
-                                        className={`cursor-pointer rounded-full px-4 py-3 text-sm w-32 bg-blue-gray-50 items-center justify-center flex font-medium md:text-base lg:px-6 ${
-                                            openTab === "events"
-                                                ? "bg-boxdark text-white"
-                                                : "text-body-color hover:bg-black hover:text-white dark:text-dark-6 dark:hover:text-white"
-                                        }`}
-                                    >
-                                        Events
-                                    </a>
-                                    <a
-                                        onClick={() => handleTabOpen("favorites")}
-                                        className={`cursor-pointer rounded-full px-4 py-3 text-sm w-32 bg-blue-gray-50 items-center justify-center flex font-medium md:text-base lg:px-6 ${
-                                            openTab === "favorites"
-                                                ? "bg-boxdark text-white"
-                                                : "text-body-color hover:bg-black hover:text-white dark:text-dark-6 dark:hover:text-white"
-                                        }`}
-                                    >
-                                        Favorites
-                                    </a>
-                                    <a
-                                        onClick={() => handleTabOpen("archived")}
-                                        className={`cursor-pointer rounded-full px-4 py-3 text-sm w-32 bg-blue-gray-50 items-center justify-center flex font-medium md:text-base lg:px-6 ${
-                                            openTab === "archived"
-                                                ? "bg-boxdark text-white"
-                                                : "text-body-color hover:bg-black hover:text-white dark:text-dark-6 dark:hover:text-white"
-                                        }`}
-                                    >
-                                        Archived
-                                    </a>
-                                </div>
-                                <Communities tabCategory="communities" open={openTab}/>
-                                <Events tabCategory="events" open={openTab}/>
-                                <Favorites tabCategory="favorites" open={openTab}/>
-                                <Archived tabCategory="archived" open={openTab}/>
-                            </div>
-                        </div>
-                    </div>
+      <section className="py-5 dark:bg-dark flex w-full justify-center m-auto">
+        <div className="container"> 
+          <div className="-mx-4 flex flex-wrap">
+            <div className="w-full px-4">
+              <div className="mb-14 w-full m-auto flex justify-center flex-col">
+                <div className="flex flex-col justify-between w-full flex-wrap rounded-full border border-blue-gray-200 px-2 py-2 dark:border-dark-3 sm:flex-row">
+                  <button
+                    onClick={() => handleTabOpen("communities")}
+                    className={`cursor-pointer rounded-full px-4 py-3 text-sm w-32  bg-blue-gray-50 items-center justify-center flex font-medium md:text-base lg:px-6 ${
+                      openTab === "communities"
+                        ? "bg-black"
+                        : "text-body-color hover:bg-black hover:text-white dark:text-dark-6 dark:hover:text-white"
+                    }`}
+                  >
+                    Communities
+                  </button>
+                  <button
+                    onClick={() => handleTabOpen("events")}
+                    className={`cursor-pointer rounded-full px-4 py-3 text-sm w-32 bg-blue-gray-50 items-center justify-center flex font-medium md:text-base lg:px-6 ${
+                      openTab === "events"
+                        ? "bg-boxdark "
+                        : "text-body-color hover:bg-black hover:text-white dark:text-dark-6 dark:hover:text-white"
+                    }`}
+                  >
+                    Events
+                  </button>
+                  <button
+                    onClick={() => handleTabOpen("favorites")}
+                    className={`cursor-pointer rounded-full px-4 py-3 text-sm w-32 bg-blue-gray-50 items-center justify-center flex font-medium md:text-base lg:px-6 ${
+                      openTab === "favorites"
+                        ? "bg-boxdark "
+                        : "text-body-color hover:bg-black hover:text-white dark:text-dark-6 dark:hover:text-white"
+                    }`}
+                  >
+                    Favorites
+                  </button>
+                  <button
+                    onClick={() => handleTabOpen("archived")}
+                    className={`cursor-pointer rounded-full px-4 py-3 text-sm w-32 bg-blue-gray-50 items-center justify-center flex font-medium md:text-base lg:px-6 ${
+                      openTab === "archived"
+                        ? "bg-boxdark "
+                        : "text-body-color hover:bg-black hover:text-white dark:text-dark-6 dark:hover:text-white"
+                    }`}
+                  >
+                    Archived
+                  </button>
                 </div>
-            </section>
+                <Communities tabCategory="communities" open={openTab} />
+                <Events tabCategory="events" open={openTab} />
+                <Favorites tabCategory="favorites" open={openTab} />
+                <Archived tabCategory="archived" open={openTab} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
